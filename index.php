20c099e97a30b08dd28efdad0c64922c4717707d
Makarenco <marcelot78@hotmail.com>
2023-01-17 05:28:28 -0500

    nuevo

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
+<?php $nombre = "UTU Solymar"; ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estamos en Debian</title>
+    <title><?php echo $nombre ?></title>
     <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
+
+<?php
     
+
+   echo "
     <h1>Estamos en Debian!!</h1>
     <h2>Nuestro primer proyecto con Debian y Git</h2>
+    <h3>Bienvenidos a $nombre</h3>
 
     <ul>
         <b><li>Instalamos GIT</li></b>
         <li>Configuramos el nombre del usuario</li>
-        <li>Configuramos el correo del usuario</li>
-    </ul>
+        <li>Configuramos el correo del usuario</li> <br><br>
+        <input type='button' value='Apretame!'>
+        
+    </ul>";
+
+?>
+
 
 </body>
 </html>
